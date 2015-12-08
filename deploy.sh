8be4a504b7f5d08f4912d04e317d3656b9dfa1a2
Dave Kaplan <dave@exygy.com>
2015-12-07 16:50:37 -0800

    added error catch to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd dist
+DIST=dist
+if [ ! -d "$DIST" ]; then
+	echo "** Uh oh! **"
+	echo "Directory '$DIST' does not exist -- you need to run 'npm start' before deploying."
+	exit
+fi
+cd $DIST
 git init
 git remote add dev git@heroku.com:sf-dahlia-pattern-library-dev.git
 git add .
