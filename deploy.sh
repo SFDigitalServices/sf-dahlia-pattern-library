26518f7ad376689eda7852a4377783e02b7d3b48
Dave Kaplan <dave@exygy.com>
2015-12-09 11:50:24 -0800

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,14 @@ if [ ! -d "$DIST" ]; then
 fi
 cd $DIST
 git init
-
 appname="$1"
-if [ ! appname ]; then
+if [ appname ]; then
 	app="sf-dahlia-pattern-library-$appname"
 	heroku create $app
 else
 	app="sf-dahlia-pattern-library"
+	git remote add heroku git@heroku.com:$app.git
 fi
-git remote add heroku git@heroku.com:$app.git
 git add .
 git commit -am 'Deploying latest pattern library to heroku'
 git push --force heroku master
