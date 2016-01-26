820e7f11908976d2489fc7dee6204adb87a16354
slowbot <jesse@slowbot.com>
2016-01-26 09:54:18 -0800

    adding styles of occupancy background color

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -19,7 +19,7 @@ window.jQuery = $;
 var Modernizr = require('./modernizr.js');
 
 // Loading foundation from bower in order to support modernizr module
-var foundation = require('foundation/js/foundation.min.js');
+var foundation = require('foundation/js/foundation.js');
 
 //require('smoothstate/jquery.smoothState.min.js');
 // var smoothState = require('./jquery.smoothState.min.js');
