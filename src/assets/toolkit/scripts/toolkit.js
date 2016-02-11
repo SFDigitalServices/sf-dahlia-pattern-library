8fec55ac2ac7e80c3ed49572d00b6921ee8801b5
slowbot <jesse@slowbot.com>
2016-02-11 11:49:10 -0800

    fixing accordion path to foundation

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -20,6 +20,7 @@ var Modernizr = require('./modernizr.js');
 
 // Loading foundation from bower in order to support modernizr module
 var foundation = require('../../vendor/foundation/js/foundation/foundation.js');
+var foundationAccordion = require('../../vendor/foundation/js/foundation/foundation.accordion.js');
 var foundationAbide = require('../../vendor/foundation/js/foundation/foundation.abide.js');
 var foundationTooltip = require('../../vendor/foundation/js/foundation/foundation.tooltip.js');
 
