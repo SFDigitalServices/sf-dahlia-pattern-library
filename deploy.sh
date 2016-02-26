635af57c8e042f73f8c746bfbc162dbe3b1bd84e
Dave Kaplan <dave@exygy.com>
2016-02-26 11:32:51 -0800

    had to update deploy.sh to please the Heroku gods

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,32 @@ if [ $appname ]; then
 else
 	app="sf-dahlia-pattern-library"
 fi
+echo '{
+  "require": {
+    "php": "^5.6.0"
+  }
+}' > composer.json
+echo '{
+    "_readme": [
+        "This file locks the dependencies of your project to a known state",
+        "Read more about it at http://getcomposer.org/doc/01-basic-usage.md#composer-lock-the-lock-file",
+        "This file is @generated automatically"
+    ],
+    "hash": "35ebe24ec8efc38b3b544041896aa986",
+    "packages": [],
+    "packages-dev": [],
+    "aliases": [],
+    "minimum-stability": "stable",
+    "stability-flags": [],
+    "prefer-stable": false,
+    "prefer-lowest": false,
+    "platform": {
+        "php": "^5.6.0"
+    },
+    "platform-dev": []
+}' > composer.lock
+echo 'web: vendor/bin/heroku-php-nginx' > Procfile
+echo 'vendor' > .gitignore
 git remote add $app https://git.heroku.com/$app.git
 git add .
 git commit -am 'Deploying latest pattern library to heroku'
