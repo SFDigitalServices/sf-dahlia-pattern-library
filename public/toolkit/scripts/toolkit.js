a4f9f8a578e60dcb66d6485054cc59c49b83c304
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-03-01 14:38:59 -0800

    Fix image paths

--- public/toolkit/scripts/toolkit.js
+++ public/toolkit/scripts/toolkit.js
@@ -189,9 +189,9 @@ angular.module('dahlia',
   })
   .controller('CarouselSampleController', ['$scope', 'Carousel', function($scope, Carousel) {
     $scope.images = [
-      '/assets/toolkit/images/property4-16x9.jpg',
-      '/assets/toolkit/images/property4-16x9.jpg',
-      '/assets/toolkit/images/property4-16x9.jpg',
+      '/dist/toolkit/images/property4-16x9.jpg',
+      '/dist/toolkit/images/property4-16x9.jpg',
+      '/dist/toolkit/images/property4-16x9.jpg',
     ]
 
     $scope.Carousel = Carousel;
